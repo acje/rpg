0fd107fc3074119130360e7ba8d306d816f5e211
Anders Jensen <anders.c.jensen@gmail.com>
2019-11-06 10:53:23 +0100

    first commit with USR schema

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# rpg
+# Rust postgres
+
+docker run --name some-postgres -e POSTGRES_PASSWORD=rpg -p 5432:5432 -d postgres
--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,52 @@
 use postgres::{Connection, TlsMode};
+use chrono::{NaiveDate};
 
-struct Person {
+struct UsrMessage {
     id: i32,
-    name: String,
-    data: Option<Vec<u8>>,
+    slaktedato: NaiveDate,
+    efta: i32,
+    message: String,
+    skrottnr: i64,
+    duplicate: bool,
 }
 
 fn main() {
-    let conn = Connection::connect("postgres://postgres@localhost:5433", TlsMode::None).unwrap();
-    conn.execute("CREATE TABLE person (
+    let conn = Connection::connect("postgres://postgres:rpg@localhost:5432", TlsMode::None).unwrap();
+
+     conn.execute("CREATE TABLE IF NOT EXISTS usrmessage (
                     id              SERIAL PRIMARY KEY,
-                    name            VARCHAR NOT NULL,
-                    data            BYTEA
+                    slaktedato      DATE NOT NULL,
+                    efta            INTEGER NOT NULL,
+                    message         VARCHAR NOT NULL,
+                    skrottnr        BIGINT NOT NULL,
+                    duplicate       BOOL DEFAULT FALSE
                   )", &[]).unwrap();
-    let me = Person {
+
+ 
+    let message = UsrMessage {
         id: 0,
-        name: "Steven".to_string(),
-        data: None,
+        slaktedato: NaiveDate::from_ymd(2015, 3, 14),
+        efta: 140,
+        message: "abc".to_string(),
+        skrottnr: 12345678,
+        duplicate: false,
     };
-    conn.execute("INSERT INTO person (name, data) VALUES ($1, $2)",
-                 &[&me.name, &me.data]).unwrap();
-    for row in &conn.query("SELECT id, name, data FROM person", &[]).unwrap() {
-        let person = Person {
+
+    conn.execute("INSERT INTO usrmessage (slaktedato, efta, message, skrottnr, duplicate) VALUES ($1, $2, $3, $4, $5)",
+                 &[&message.slaktedato, &message.efta, &message.message , &message.skrottnr , &message.duplicate]).unwrap();
+
+    for row in &conn.query("SELECT id, slaktedato, efta, message, skrottnr, duplicate FROM usrmessage", &[]).unwrap() {
+        let usrmessage = UsrMessage {
             id: row.get(0),
-            name: row.get(1),
-            data: row.get(2),
+            slaktedato: row.get(1),
+            efta: row.get(2),
+            message: row.get(3),
+            skrottnr: row.get(4),
+            duplicate: row.get(5),
         };
-        println!("Found person {}", person.name);
+        println!("id: {0}, slaktedato: {1}, efta: {2}, message: {3}, skrottnr: {4}, duplicate: {5}", usrmessage.id, usrmessage.slaktedato, usrmessage.efta, usrmessage.message, usrmessage.skrottnr, usrmessage.duplicate);
+        if usrmessage.id==10 {
+            conn.execute("DROP TABLE usrmessage", &[]).unwrap();
+        };   
     }
 }
