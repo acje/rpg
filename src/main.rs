0f92665b4a545536638b9e0280eb79695264d264
Anders Jensen <anders.c.jensen@gmail.com>
2019-11-06 14:29:14 +0100

    rearrange struct

--- src/main.rs
+++ src/main.rs
@@ -1,51 +1,63 @@
 use postgres::{Connection, TlsMode};
 use chrono::{NaiveDate};
+use std::collections::HashMap;
 
 struct UsrMessage {
     id: i32,
+    message: String,
     slaktedato: NaiveDate,
     efta: i32,
-    message: String,
     skrottnr: i64,
     duplicate: bool,
 }
 
 fn main() {
-    let conn = Connection::connect("postgres://postgres:rpg@localhost:5432", TlsMode::None).unwrap();
+    
+    // Hashmap for detection of duplicate messages by (slaktedato, efta, skrottnr)
+    let mut map: HashMap<&(NaiveDate, i32, i64), bool> = HashMap::new();
 
+    let conn = Connection::connect("postgres://postgres:rpg@localhost:5432", TlsMode::None).unwrap();
+    
      conn.execute("CREATE TABLE IF NOT EXISTS usrmessage (
                     id              SERIAL PRIMARY KEY,
+                    message         VARCHAR NOT NULL,
                     slaktedato      DATE NOT NULL,
                     efta            INTEGER NOT NULL,
-                    message         VARCHAR NOT NULL,
                     skrottnr        BIGINT NOT NULL,
                     duplicate       BOOL DEFAULT FALSE
                   )", &[]).unwrap();
 
- 
     let message = UsrMessage {
         id: 0,
+        message: "abc".to_string(),
         slaktedato: NaiveDate::from_ymd(2015, 3, 14),
         efta: 140,
-        message: "abc".to_string(),
         skrottnr: 12345678,
         duplicate: false,
     };
 
-    conn.execute("INSERT INTO usrmessage (slaktedato, efta, message, skrottnr, duplicate) VALUES ($1, $2, $3, $4, $5)",
-                 &[&message.slaktedato, &message.efta, &message.message , &message.skrottnr , &message.duplicate]).unwrap();
+    conn.execute("INSERT INTO usrmessage (message, slaktedato, efta, skrottnr, duplicate) VALUES ($1, $2, $3, $4, $5)",
+                 &[&message.message, &message.slaktedato, &message.efta, &message.skrottnr , &message.duplicate]).unwrap();
 
-    for row in &conn.query("SELECT id, slaktedato, efta, message, skrottnr, duplicate FROM usrmessage", &[]).unwrap() {
+    for row in &conn.query("SELECT id, message, slaktedato, efta, skrottnr, duplicate FROM usrmessage", &[]).unwrap() {
         let usrmessage = UsrMessage {
             id: row.get(0),
-            slaktedato: row.get(1),
-            efta: row.get(2),
-            message: row.get(3),
+            message: row.get(1),
+            slaktedato: row.get(2),
+            efta: row.get(3),
             skrottnr: row.get(4),
             duplicate: row.get(5),
         };
-        println!("id: {0}, slaktedato: {1}, efta: {2}, message: {3}, skrottnr: {4}, duplicate: {5}", usrmessage.id, usrmessage.slaktedato, usrmessage.efta, usrmessage.message, usrmessage.skrottnr, usrmessage.duplicate);
-        if usrmessage.id==10 {
+        println!("id: {0}, message: {1}, slaktedato: {2}, efta: {3}, skrottnr: {4}, duplicate: {5}", usrmessage.id, usrmessage.message, usrmessage.slaktedato, usrmessage.efta, usrmessage.skrottnr, usrmessage.duplicate);
+        // hash
+
+
+        conn.execute("UPDATE usrmessage SET duplicate = true WHERE id = $1",
+                 &[&message.id]).unwrap();
+
+        println!("id: {0}, message: {1}, slaktedato: {2}, efta: {3}, skrottnr: {4}, duplicate: {5}", usrmessage.id, usrmessage.message, usrmessage.slaktedato, usrmessage.efta, usrmessage.skrottnr, usrmessage.duplicate);
+        
+        if usrmessage.id==5 {
             conn.execute("DROP TABLE usrmessage", &[]).unwrap();
         };   
     }
